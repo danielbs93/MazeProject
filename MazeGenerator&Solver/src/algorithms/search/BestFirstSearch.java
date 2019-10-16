a4871cc5ee4f394749ec56b34422b1e3b197151a
danielbs93 <49146507+danielbs93@users.noreply.github.com>
2019-10-16 21:50:56 +0300

    Update BestFirstSearch.java

--- MazeGenerator&Solver/src/algorithms/search/BestFirstSearch.java
+++ MazeGenerator&Solver/src/algorithms/search/BestFirstSearch.java
@@ -2,6 +2,10 @@ package algorithms.search;
 
 /**
  * Created by Daniel Ben Simon
+ * Best search algorithm builds a path with diagonals moves only if there is a vertical&horizontal path to that cell. 
+ * For example:  0,0,1
+ *               1,0,0  --> cell[0,0] to cell [1,1] legal diagonal move
+ *               0,1,0  --> cell[1,1] to cell [0,2] illegal diagonal move
  */
 
 import java.util.PriorityQueue;
